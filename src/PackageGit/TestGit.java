0d2f5bc03512982a6ea5d089b020e07232f70e57
Zaychenko <Zaychenko@10.91.129.56>
2018-03-17 12:18:49 +0100

    Ajouter main

--- src/PackageGit/TestGit.java
+++ src/PackageGit/TestGit.java
@@ -8,9 +8,15 @@ public class TestGit {
 		this.gitFromScratch = parameter;
 	}
 	
-	public String  retunrt();{
-	return  this.gitFromScratch();
+	public String  retunrt(){
+	return  this.gitFromScratch;
 	}
+	
+	
+	public static void main(String [] args) {
+		System.out.println("Hello");
+	}
+	
 	
 	
 }
