0ef64d18c257d65c09aa58a4803c780cc2303c04
Zaychenko <Zaychenko@10.91.129.56>
2018-03-17 12:27:28 +0100

    Test commit avec push

--- src/PackageGit/TestGit.java
+++ src/PackageGit/TestGit.java
@@ -12,14 +12,7 @@ public class TestGit {
 	return  this.gitFromScratch;
 	}
 	
-	public static void main(String [] args) {
-		
-	}
-	
-	
-	public static void main(String [] args) {
-		System.out.println("Hello");
-	}
+
 	
 	
 	
