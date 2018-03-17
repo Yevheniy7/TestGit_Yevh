3bd1f8d38818efb1670047d1d1ecb394a9cb94b2
Victor <Victor@10.90.129.10>
2018-03-17 13:01:57 +0100

    tiens

---
+++ src/PackageGit/MainGit.java
@@ -0,0 +1,16 @@
+package PackageGit;
+
+public class MainGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		TestGit2 test = new TestGit2("parametre");
+		
+		System.out.println(test.retunrt());
+		
+		
+	}
+
+}
---
+++ src/PackageGit/TestGit2.java
@@ -0,0 +1,13 @@
+package PackageGit;
+
+public class TestGit2 extends TestGit {
+
+	public TestGit2(String parameter) {
+		super(parameter);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+}
